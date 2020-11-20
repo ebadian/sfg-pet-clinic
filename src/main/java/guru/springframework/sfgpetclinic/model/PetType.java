c7cb14e91cb10a7324be0e8a8d22f35f23a631e5
ebadiana <a.ebadian@elsevier.com>
2020-11-20 03:59:33 +0000

    Creating the initial POJOs

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    public String getGetType() {
+        return getType;
+    }
+
+    public void setGetType(String getType) {
+        this.getType = getType;
+    }
+
+    private String getType;
+}
