aba6da3f3d04770a6e592ed1afe9d939f168dce6
Ali Ebadian <50441492+ebadian@users.noreply.github.com>
2020-11-22 17:15:55 +0000

    List owners in Index page (#13)
    - Modified VetsController to have VetsSerive injected from constructor.
     - Added Model to index method of the controller
     - added the list of Vets to the model from Vetservice
     - Added table to index.html, thyleaf will loop over each vets and display them

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetsController {
 
-    @RequestMapping({"/vets","/vets.html", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
