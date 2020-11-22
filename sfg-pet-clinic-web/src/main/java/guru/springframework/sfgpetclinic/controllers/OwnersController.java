7352a3deb439776d37899f03c90ef396563290c7
Ali Ebadian <50441492+ebadian@users.noreply.github.com>
2020-11-22 17:07:35 +0000

    List owners in Index page (#12)
    - Modified OwnersController to have OwnersSerive injected from constructor.
     - Added Model to index method of the controller
     - added the list of Owners to the model from ownerservice
     - Added table to index.html, thyleaf will loop over each owner and display them

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnersController {
 
-    @RequestMapping({"/owners","/owners.html","/owners/index", "owners/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+    @RequestMapping({"/owners", "/owners.html", "/owners/index", "owners/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
