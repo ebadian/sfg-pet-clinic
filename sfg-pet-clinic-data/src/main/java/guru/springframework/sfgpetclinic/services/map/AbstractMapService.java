a72b368f95378d8d1977579037dfdac300d6f5e1
ebadiana <a.ebadian@elsevier.com>
2020-11-23 14:40:43 +0000

    Let MapServices mimic JPA repos  - Save method calculates Id based on the size of map.  - In AbstractMapService the Generics extend BaseEntity and Long.  - Refactored Save method just to take in object, rather than id and object.

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,20 +1,43 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService <T,ID> {
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService <T extends BaseEntity,ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save( T object) {
+        if(object != null) {
+            object.setId(getNextId());
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object can not be null");
+        }
         return object;
+    }
+
+
+    private Long getNextId(){
+
+        Long nextId = null;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 
     void deleteById(ID id) {
