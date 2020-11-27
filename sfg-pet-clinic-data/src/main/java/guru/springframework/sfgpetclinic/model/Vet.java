5c207d52dffef3067607f3a02cee5ff40a1b57ad
ebadiana <a.ebadian@elsevier.com>
2020-11-27 12:00:32 +0000

    Added Speciality to data load:  - Renamed SpecialityService  - Instantiated specialities field in Vet class.  - Changed speciality set methods.  - Added logic to VetServiceMap to save Vets with speciality.

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,15 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person {
+    private Set<Speciality> specialities = new HashSet<>();
+
     public Set<Speciality> getSpecialities() {
         return specialities;
     }
 
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
+    public void addSpeciality(Speciality speciality) {
+        this.specialities.add(speciality);
     }
 
-    private Set<Speciality> specialities;
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities.addAll(specialities);
+    }
 }
